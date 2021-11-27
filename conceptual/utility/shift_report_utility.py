38e203f7aebf91535dbe031dbd13e3e43c38cb91
Daizy <DaizyModi>
2021-11-27 12:43:29 +0530

    changes

--- conceptual/utility/shift_report_utility.py
+++ conceptual/utility/shift_report_utility.py
@@ -7,9 +7,10 @@ def calculate_time_diff(name):
     doc = frappe.get_doc("Shift Report", name)
     for row in doc.breakdown_detail:
         if row.from_time and row.to_time:
-            time_1 = datetime.strptime(row.from_time, "%H:%M:%S")
-            time_2 = datetime.strptime(row.to_time, "%H:%M:%S")
+            time_1 = datetime.strptime(str(row.from_time), "%H:%M:%S")
+            time_2 = datetime.strptime(str(row.to_time), "%H:%M:%S")
 
             time_diff = time_2 - time_1
             time_diff_in_mins = time_diff.seconds / 60
             row.time_in_mins = time_diff_in_mins
+    return time_diff_in_mins
