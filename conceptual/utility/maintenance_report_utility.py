c48776f979a9f0b482937c2911308acdb112c32b
Daizy <DaizyModi>
2021-12-01 13:49:46 +0530

    changes

--- conceptual/utility/maintenance_report_utility.py
+++ conceptual/utility/maintenance_report_utility.py
@@ -5,8 +5,10 @@ import frappe
 def get_maintenance_checklist(template):
     maintenance_checklist_template = frappe.get_doc(
         "Maintenance Checklist Template", template)
+
     checklist = []
-    for row in maintenance_checklist_template:
+
+    for row in maintenance_checklist_template.maintenance_checklist:
         checklist.append({
             'checklist': row.checklist
         })
