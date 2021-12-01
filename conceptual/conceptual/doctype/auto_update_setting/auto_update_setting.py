a0377bfd052fbcd570aeb597bab3be77811a94f9
Daizy <DaizyModi>
2021-12-01 16:16:55 +0530

    auto update settings

---
+++ conceptual/conceptual/doctype/auto_update_setting/auto_update_setting.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2021, Jigar Tarpara and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class AutoUpdateSetting(Document):
+	pass
